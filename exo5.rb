7ecda3cee070af291ad0d1f54a6ae54405ce059c
Qais17 <qaisbewebpro@gmail.com>
2016-12-28 16:31:25 +0100

    maj 5.0.0.1

---
+++ exo5.rb
@@ -0,0 +1,17 @@
+
+mots = ""
+print "Propose un mot; je te dirai si c'est un palindrome ! : \n\r"
+mots = gets
+ i = 0 
+ j = mots.length-1 
+ while i < (mots.length)/2
+     
+     if mots[i] == mots[j]
+         i +=1
+         j -=1
+        else puts "non"
+            exit        
+     end 
+    
+ end
+ puts "oui"
